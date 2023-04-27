dd1cd0e87a8f04f7dc95eca3db591b031a1d4bfa
JoaoPedro <joaopedro1172@gmail.com>
2023-04-27 15:41:42 -0300

    auth using token and tenant

--- api/api.ts
+++ api/api.ts
@@ -1,27 +1,41 @@
 import express from 'express'
 import * as contentDisposition from 'content-disposition'
 import * as fs from 'fs'
+import { config as dotenvConfig } from "dotenv-safe"
 
 const app = express()
+dotenvConfig()
 
-app.post('/upload/', (req: express.Request, res: express.Response) => {
+const authUrl = process.env.URL_AUTH_SERVICE;
+
+app.post('/upload/:token/:tenant', (req: express.Request, res: express.Response) => {
     if (req.headers['content-type'] === 'application/octet-stream') {
-        let data: Buffer[] = []
-        const filename: string = getFilename(req)
+        const authParams = new URLSearchParams({
+            token: req.params.token,
+            tenant: req.params.tenant
+        });
 
-        req.on('data', (chunk: Buffer) => data.push(chunk))
+        let data: Buffer[] = [];
+        const filename: string = getFilename(req);
 
-        req.on('end', () => {
-            fs.writeFile(`./uploads/${filename}`, Buffer.concat(data), (err: NodeJS.ErrnoException | null) => {
-                if (err) {
-                    console.error(err)
-                    res.status(500).send(`Error saving file ${filename}`)
-                } else {
-                    console.log(`File ${filename} saved`)
-                    res.end(`File ${filename} saved.`)
+        fetch(`${authUrl}?${authParams}`)
+            .then((response) => {
+                if (response.status == 200) {
+                    req.on('data', (chunk: Buffer) => data.push(chunk))
+
+                    req.on('end', () => {
+                        fs.writeFile(`./uploads/${filename}`, Buffer.concat(data), (err: NodeJS.ErrnoException | null) => {
+                            if (err) {
+                                console.error(err)
+                                res.status(500).send(`Error saving file ${filename}`)
+                            } else {
+                                console.log(`File ${filename} saved`)
+                                res.end(`File ${filename} saved.`)
+                            }
+                        })
+                    })
                 }
             })
-        })
     } else {
         res.status(400).end('Bad Request')
     }
